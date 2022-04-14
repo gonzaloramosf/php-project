01484f02ffec79d1b00b88a8b6d0fc6262d57e2f
gonzaloramosf <gonza.farinho@hotmail.com>
2022-04-14 16:18:37 -0300

    first commit

---
+++ main.php
@@ -0,0 +1,48 @@
+<?php
+    $routes = [
+        'Home' => [
+            'title' => 'Home page',
+        ],
+        'Products' => [
+            'title' => 'Products',
+        ],
+        'Login' => [
+            'title' => 'Login',
+        ],
+        'Register' => [
+            'title' => 'Register',
+        ],
+        'NotFoundPage' => [
+            'title' => 'Not found page',
+        ],
+    ];
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <!--<span class="logo"> logo </span>-->
+    </header>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Products</li>
+            <li>Login</li>
+            <li>Register</li>
+        </ul>
+    </nav>
+    <main>
+        <?php
+
+        ?>
+    </main>
+</body>
+</html>
